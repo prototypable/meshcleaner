9399231cb2329ae2f9e69a96ba30a3d829f83a44
Pontus Johannesson <hydrardraconis@gmail.com>
2012-08-02 00:35:01 +0200

    Implemented basic functionality in meshcleaner

--- meshcleaner.cpp
+++ meshcleaner.cpp
@@ -1,7 +1,49 @@
-#include "mesh_definitions.hpp"
 #include <iostream>
+#include <sstream>
+#include "mesh_definitions.hpp"
+#include "meshcleanerworker.hpp"
+
+std::string file_basename(const std::string & fn)
+{
+    std::string::size_type index = fn.rfind('.');
+    if (index != std::string::npos)
+        return fn.substr(0, index);
+    else
+        return fn;
+}
 
 int main(int argc, char * argv[])
 {
-    std::cout << "Placeholder\n";
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " file.stl" << std::endl;
+        return -1;
+    }
+    MeshcleanerWorker Worker;
+    Worker.LoadSTL(argv[1]);
+    if (!Worker.IsLoaded())
+    {
+        std::cerr << "Cannot open file " << argv[1] << std::endl;
+        return -1;
+    }
+
+    if (Worker.IsManifold())
+    {
+        std::cerr << "File is already manifold and doesn't need cleaning" << std::endl;
+        return 0;
+    }
+
+    Worker.Clean();
+
+    if (Worker.IsManifold())
+    {
+        std::cerr << "File cleaned successfully, writing result to " << file_basename(argv[1]) << "_fixed.stl" << std::endl;
+        std::stringstream fn;
+        fn << file_basename(argv[1]) << "_fixed.stl";
+        Worker.WriteSTL(fn.str());
+        return 0;
+    }
+
+    std::cout << "Cleaning failed" << std::endl;
+    return 1;
 }
--- meshcleanerworker.cpp
+++ meshcleanerworker.cpp
@@ -77,6 +77,7 @@ void MeshcleanerWorker::GenerateTopology()
     vcg::tri::Clean<Mesh>::RemoveDuplicateFace(STL);
     vcg::tri::Clean<Mesh>::RemoveUnreferencedVertex(STL);
     vcg::tri::Clean<Mesh>::RemoveZeroAreaFace(STL);
+    vcg::tri::Clean<Mesh>::RemoveDegenerateFace(STL);
 
     vcg::tri::UpdateTopology<Mesh>::FaceFace(STL);
     TopologyGenerated = true;
