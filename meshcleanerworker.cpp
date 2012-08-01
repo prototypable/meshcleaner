ae0542d1b5293995fe420e39df4e5f3247470ee9
Pontus Johannesson <hydrardraconis@gmail.com>
2012-08-01 19:31:06 +0200

    Initial implementation of worker object

---
+++ meshcleanerworker.cpp
@@ -0,0 +1,94 @@
+#include "meshcleanerworker.hpp"
+#include <string>
+// Includes for STL import
+#include <wrap/io_trimesh/io_mask.h>
+#include <wrap/io_trimesh/import_stl.h>
+#include <wrap/io_trimesh/export_stl.h>
+// Include cleaning and topology headers
+#include <vcg/complex/algorithms/clean.h>
+#include <vcg/complex/algorithms/update/topology.h>
+#include <vcg/complex/append.h>
+
+MeshcleanerWorker::MeshcleanerWorker()
+{
+    STLLoaded = false;
+    TopologyGenerated = false;
+}
+
+MeshcleanerWorker::MeshcleanerWorker(std::string stl_fn)
+{
+    TopologyGenerated = false;
+    LoadSTL(stl_fn);
+}
+
+bool MeshcleanerWorker::LoadSTL(std::string stl_fn)
+{
+    ErrorDescription.clear();
+    STL.Clear();
+    int error = vcg::tri::io::ImporterSTL<Mesh>::Open(STL, stl_fn.c_str());
+    if (error != 0)
+    {
+        ErrorDescription = vcg::tri::io::ImporterSTL<Mesh>::ErrorMsg(error);
+        STLLoaded = false;
+        return false;
+    }
+    STLLoaded = true;
+
+    vcg::tri::Clean<Mesh>::RemoveDuplicateVertex(STL);
+
+    // Force garbage collection on vectors to free some memory
+    vcg::tri::Allocator<Mesh>::CompactVertexVector(STL);
+    vcg::tri::Allocator<Mesh>::CompactFaceVector(STL);
+    return true;
+}
+
+bool MeshcleanerWorker::LoadFromMesh(Mesh & inputmesh)
+{
+    ErrorDescription.clear();
+    STL.Clear();
+    vcg::tri::Append<Mesh, Mesh>::Mesh(STL, inputmesh);
+    return true;
+}
+
+bool MeshcleanerWorker::WriteSTL(std::string fn, bool binary)
+{
+    vcg::tri::io::ExporterSTL<Mesh>::Save(STL, fn.c_str(), binary);
+}
+
+MeshcleanerWorker::~MeshcleanerWorker()
+{
+
+}
+
+bool MeshcleanerWorker::IsManifold()
+{
+    if (!TopologyGenerated)
+        GenerateTopology();
+    if (vcg::tri::Clean<Mesh>::CountNonManifoldEdgeFF(STL) > 0)
+        return false;
+    else
+        return true;
+}
+
+void MeshcleanerWorker::GenerateTopology()
+{
+    // Clean out duplicate faces, unreferenced vertices and zero area faces to prevent
+    // topology generation from crashing
+    vcg::tri::Clean<Mesh>::RemoveDuplicateFace(STL);
+    vcg::tri::Clean<Mesh>::RemoveUnreferencedVertex(STL);
+    vcg::tri::Clean<Mesh>::RemoveZeroAreaFace(STL);
+
+    vcg::tri::UpdateTopology<Mesh>::FaceFace(STL);
+    TopologyGenerated = true;
+}
+
+void MeshcleanerWorker::Clean()
+{
+    if(!TopologyGenerated)
+        GenerateTopology();
+    // Cleanout non-manifold faces and vertices, if any is removed, rebuild topology after
+    if (vcg::tri::Clean<Mesh>::RemoveNonManifoldFace(STL) > 0)
+        vcg::tri::UpdateTopology<Mesh>::FaceFace(STL);
+    if (vcg::tri::Clean<Mesh>::RemoveNonManifoldVertex(Mesh) > 0)
+        vcg::tri::UpdateTopology<Mesh>::FaceFace(STL);
+}
---
+++ meshcleanerworker.hpp
@@ -0,0 +1,37 @@
+#ifndef MESHCLEANERWORKER_HPP
+#define MESHCLEANERWORKER_HPP
+#include "mesh_definitions.hpp"
+
+class MeshcleanerWorker
+{
+public:
+    MeshcleanerWorker();
+    // Loads a file while constructing the worker
+    MeshcleanerWorker(std::string stl_fn);
+    ~MeshcleanerWorker();
+
+    // Loads a STL file
+    bool LoadSTL(std::string fn);
+    // Loads a mesh directly from memory
+    bool LoadFromMesh(Mesh & inputmesh);
+    // Writes out a stl, as binary by default but can be overridden
+    bool WriteSTL(std::string fn, bool binary = true);
+
+    // Check if a STL file is loaded into the worker
+    bool IsLoaded() const { return STLLoaded; }
+    // Get the last error
+    const std::string & GetError() const { return ErrorDescription; }
+
+    bool IsManifold() const;
+
+    void GenerateTopology();
+
+    void Clean();
+protected:
+    Mesh STL;
+    bool STLLoaded;
+    bool TopologyGenerated;
+    std::string ErrorDescription;
+};
+
+#endif // MESHCLEANERWORKER_HPP
