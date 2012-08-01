746e51738b4e77efe3fce12fd974e8286c95c612
Pontus Johannesson <hydrardraconis@gmail.com>
2012-08-01 19:53:59 +0200

    Removed testing code from meshcleaner_rest_server which now is rewritten inside the MeshcleanerWorker class

--- meshcleaner_rest_server.cpp
+++ meshcleaner_rest_server.cpp
@@ -16,43 +16,6 @@ using boost::asio::ip::tcp;
 
 int main(int argc, char * argv[])
 {
-    boost::asio::io_service io_service;
-
-    Mesh MyMesh;
-    int errorcode = vcg::tri::io::ImporterSTL<Mesh>::Open(MyMesh, "test.stl");
-    if (errorcode != 0)
-    {
-        std::string error = vcg::tri::io::ImporterSTL<Mesh>::ErrorMsg(errorcode);
-        std::cerr << "An error occured while loading test.stl: " << error << std::endl;
-        return -1;
-    }
-
-    std::cout << "Mesh test.stl contains " << MyMesh.vn << " Vertices and " << MyMesh.fn << " facets\n";
-
-    std::cout << "Removing duplicated vertices...\n";
-    int removed = vcg::tri::Clean<Mesh>::RemoveDuplicateVertex(MyMesh);
-    std::cout << "Removing duplicated faces...\n";
-    vcg::tri::Clean<Mesh>::RemoveDuplicateFace(MyMesh);
-    std::cout << "Removing Unreferenced vertices...\n";
-    vcg::tri::Clean<Mesh>::RemoveUnreferencedVertex(MyMesh);
-    std::cout << "Removing Zero-area faces...\n";
-    vcg::tri::Clean<Mesh>::RemoveZeroAreaFace(MyMesh);
-
-
-    vcg::tri::UpdateTopology<Mesh>::FaceFace(MyMesh);
-    std::cout << "Removing Non-manifold faces...\n";
-    vcg::tri::Clean<Mesh>::RemoveNonManifoldFace(MyMesh);
-    vcg::tri::UpdateTopology<Mesh>::FaceFace(MyMesh);
-    vcg::tri::Clean<Mesh>::RemoveNonManifoldVertex(MyMesh);
-    vcg::tri::UpdateTopology<Mesh>::FaceFace(MyMesh);
-
-    vcg::tri::Allocator<Mesh>::CompactVertexVector(MyMesh);
-    vcg::tri::Allocator<Mesh>::CompactFaceVector(MyMesh);
-    std::cout << "Mesh now contains " << MyMesh.VertexNumber() << " Vertices and " << MyMesh.fn << " facets\n";
-    std::cout << "Mesh has " << vcg::tri::Clean<Mesh>::CountNonManifoldEdgeFF(MyMesh) << " Non-manifold edges and " <<
-                vcg::tri::Clean<Mesh>::CountNonManifoldVertexFF(MyMesh) << "Non-manifold-vertices\n";
-
-    vcg::tri::io::ExporterSTL<Mesh>::Save(MyMesh, "out.stl", true);
     return 0;
 }
 
