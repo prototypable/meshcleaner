250a28c53789f57253252bdb36c7073d447e4ea8
Pontus Johannesson <hydrardraconis@gmail.com>
2012-08-08 23:02:44 +0200

    Reimplemented return value "quiet mode" operation in manifoldtest

--- manifoldtest.cpp
+++ manifoldtest.cpp
@@ -1,3 +1,4 @@
+#include <string>
 // Include mesh definitions from meshcleaner
 #include "../mesh_definitions.hpp"
 
@@ -9,15 +10,27 @@
 // Mesh cleanup algorithms
 #include <vcg/complex/algorithms/clean.h>
 
+bool getoption(int argc, char * argv[], std::string option)
+{
+    for(int i = 0; i < argc; ++i)
+    {
+        std::string temp = argv[i];
+        if (temp.compare(option) == 0)
+            return true;
+    }
+    return false;
+}
+
 enum errorcodes {
-    ERROR_NONMANIFOLD_EDGE = 2,
+    ERROR_NONMANIFOLD_EDGE = 1,
     ERROR_HOLES = 4,
     ERROR_SELF_INTERSECT = 8
 };
 
 int main(int argc, char * argv[])
 {
-    if(argc != 2)
+    bool quietmode = getoption(argc, argv, "-q");
+    if((!quietmode && argc != 2) || (argc != 3 && quietmode))
     {
         std::cerr << "Usage: manifoldtest file.stl\n"
                      "Tests for and prints a number of problems that's known to cause problems with slicing" << std::endl;
@@ -25,11 +38,16 @@ int main(int argc, char * argv[])
     }
 
     Mesh stl;
-    int errorcode = vcg::tri::io::ImporterSTL<Mesh>::Open(stl, argv[1]);
+    std::string filename;
+    if(quietmode)
+        filename = argv[2];
+    else
+        filename = argv[1];
+    int errorcode = vcg::tri::io::ImporterSTL<Mesh>::Open(stl, filename.c_str());
     if (errorcode != 0)
     {
         std::string error = vcg::tri::io::ImporterSTL<Mesh>::ErrorMsg(errorcode);
-        std::cerr << "An error occured while loading " << argv[1] << ": " << error << std::endl;
+        std::cerr << "An error occured while loading " << filename << ": " << error << std::endl;
         return -1;
     }
 
@@ -47,16 +65,28 @@ int main(int argc, char * argv[])
     // Perform the actual checking
     int NonManifoldEdges = vcg::tri::Clean<Mesh>::CountNonManifoldEdgeFF(stl);
     int NonManifoldVertices = vcg::tri::Clean<Mesh>::CountNonManifoldVertexFF(stl);
-    int Holes = vcg::tri::Clean<Mesh>::CountHoles(stl);
+    int Holes = 0;
+    if (NonManifoldEdges == 0) Holes = vcg::tri::Clean<Mesh>::CountHoles(stl);
+    else
+        std::cout << "Hole detection skipped, mesh is not manifold\n";
     std::vector<Mesh::FaceType *> SelfIntersectList;
     stl.face.EnableMark();
     vcg::tri::Clean<Mesh>::SelfIntersections(stl, SelfIntersectList);
     stl.face.DisableMark();
     int SelfIntersections = SelfIntersectList.size();
-
-    std::cout << "Model has " << NonManifoldEdges << " non-manifold edges and "
-              << NonManifoldVertices << " non-manifold vertices" << std::endl;
-    std::cout << SelfIntersections << " self-intersecting faces and "
+    if (!quietmode)
+        std::cout << "Model has " << NonManifoldEdges << " non-manifold edges and "
+              << NonManifoldVertices << " non-manifold vertices" << std::endl
+              << SelfIntersections << " self-intersecting faces and "
               << Holes << " holes" << std::endl;
 
+    // Calculate return value
+    int returnvalue = 0;
+    if (NonManifoldEdges > 0)
+        returnvalue |= ERROR_NONMANIFOLD_EDGE;
+    if (Holes > 0)
+        returnvalue |= ERROR_HOLES;
+    if (SelfIntersections > 0)
+        returnvalue |= ERROR_SELF_INTERSECT;
+    return returnvalue;
 }
