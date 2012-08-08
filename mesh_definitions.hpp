2dcb369dbc03ed5b1d717cb1fc8382f9de2b3f8b
Pontus Johannesson <hydrardraconis@gmail.com>
2012-08-08 19:42:55 +0200

    Updated mesh definitions to use optional components for FaceMark, regular ones seems broken, added Face normals

--- mesh_definitions.hpp
+++ mesh_definitions.hpp
@@ -2,11 +2,13 @@
 #define MESH_DEFINITIONS_HPP
 
 #include <vector>
+#include <typeinfo>
 
 #include <vcg/simplex/vertex/base.h>
 #include <vcg/simplex/vertex/component.h>
 #include <vcg/simplex/face/base.h>
 #include <vcg/simplex/face/component.h>
+#include <vcg/simplex/face/component_ocf.h>
 
 #include <vcg/complex/complex.h>
 
@@ -18,8 +20,9 @@ class UsedTypes : public vcg::UsedTypes< vcg::Use<Vertex>::AsVertexType,
                                          vcg::Use<Edge>::AsEdgeType,
                                          vcg::Use<Face>::AsFaceType > {};
 class Vertex : public vcg::Vertex<UsedTypes, vcg::vertex::Coord3d, vcg::vertex::Normal3f, vcg::vertex::BitFlags> {};
-class Face : public vcg::Face<UsedTypes, vcg::face::VertexRef, vcg::face::BitFlags, vcg::face::FFAdj> {};
 class Edge : public vcg::Edge<UsedTypes, vcg::edge::VertexRef, vcg::edge::BitFlags> {};
-class Mesh : public vcg::tri::TriMesh< std::vector<Vertex>, std::vector<Face> > {};
+class Face : public vcg::Face<UsedTypes, vcg::face::VertexRef, vcg::face::BitFlags, vcg::face::FFAdj, vcg::face::Normal3f, vcg::face::InfoOcf, vcg::face::MarkOcf> {};
+class Mesh : public vcg::tri::TriMesh< std::vector<Vertex>, vcg::face::vector_ocf<Face>, std::vector<Edge> > {};
+
 
 #endif // MESH_DEFINITIONS_HPP
